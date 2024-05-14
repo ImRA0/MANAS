de21be5fed8600d4b64301a1932b4f699347f419
MANAS <168719474+ImRA0@users.noreply.github.com>
2024-05-14 14:22:17 +0500

    Add files via upload

---
+++ MANAS/ManasMamai/ManasMamai/cmd/web/handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
+}
---
+++ MANAS/ManasMamai/ManasMamai/go.mod
@@ -0,0 +1,3 @@
+module snippet
+
+go 1.22.3
